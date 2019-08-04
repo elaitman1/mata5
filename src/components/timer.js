5f4daddbcf80084bf777d1ef1db6f1eb1e95fb4c
Danny Peng <dannyw.peng@gmail.com>
2019-08-04 01:23:24 -0700

    got scrolling for timer to work properly in changing color of the currently selected value

--- src/components/timer.js
+++ src/components/timer.js
@@ -7,14 +7,28 @@ export default class Timer extends Component {
     minutes: null,
     seconds: null,
     showConfirmation: false
-  }
+  };
 
   toggleConfirmation = () => {
     this.setState({ showConfirmation: !this.state.showConfirmation });
-  }
+  };
 
   handleStartTimer = () => {
     this.toggleConfirmation();
+  };
+
+  updateValue = () => {
+    let selector = document.getElementById("selector").getBoundingClientRect();
+    let hourValues = document.querySelectorAll(".timer-hour-value");
+    hourValues.forEach(val => {
+      if ((val.getBoundingClientRect().top + val.getBoundingClientRect().bottom)/2 >= selector.top && (val.getBoundingClientRect().top + val.getBoundingClientRect().bottom)/2 <= selector.bottom) {
+        val.className = "timer-hour-value selected";
+      } else {
+        if (val.className !== "timer-hour-value") {
+          val.className = "timer-hour-value";
+        }
+      }
+    })
   }
 
   renderTask = () => {
@@ -25,39 +39,63 @@ export default class Timer extends Component {
           hideTask={this.props.hideTask}
           toggleConfirmation={this.toggleConfirmation}
         />
-      )
+      );
     } else {
       return (
         <div className="timer-container">
           <div className="timer-specs-container">
-            <div className="timer-scrollable-container">
-              {
-                [...Array(13).keys()].map((hr, idx) => {
-                  const hrs = hr > 1 ? "hrs" : "hr";
-                  return <p key={idx}>{hr} {hrs}</p>
-                })
-              }
+            <span className="timer-selector-bar" id="selector"></span>
+            <div className="timer-scrollable-container" id="test" onScroll={this.updateValue}>
+              {[" ", " ", " ", " ", " ", " ", " ", " "]
+                .concat([...Array(13).keys()])
+                .concat([" ", " ", " ", " ", " ", " ", " ", " ", " "])
+                .map((hr, idx) => {
+                  const hrs = hr === " " ? " " : hr > 1 ? "hrs" : "hr";
+                  const className = hr === 0 ? "timer-hour-value selected" : "timer-hour-value";
+                  return (
+                    <p key={idx} id={idx} className={className}>
+                      {hr} {hrs}
+                    </p>
+                  );
+                })}
             </div>
             <div className="timer-scrollable-container">
-              {
-                [...Array(60).keys()].map((min, idx) => (
-                  <p key={idx}>{min}</p>
-                ))
-              }
+              {[" ", " ", " ", " ", " ", " ", " ", " "]
+                .concat([...Array(60).keys()])
+                .concat([" ", " ", " ", " ", " ", " ", " ", " ", " "])
+                .map((min, idx) => {
+                  const mins = min === " " ? " " : min > 1 ? "mins" : "min";
+                  return (
+                    <p key={idx} id={idx} className="timer-value">
+                      {min} {mins}
+                    </p>
+                  );
+                })}
             </div>
             <div className="timer-scrollable-container">
-              {
-                [...Array(60).keys()].map((sec, idx) => (
-                  <p key={idx}>{sec}</p>
-                ))
-              }
+              {[" ", " ", " ", " ", " ", " ", " ", " "]
+                .concat([...Array(60).keys()])
+                .concat([" ", " ", " ", " ", " ", " ", " ", " ", " "])
+                .map((sec, idx) => {
+                  const secs = sec === " " ? " " : "sec";
+                  return (
+                    <p key={idx} id={idx} className="timer-value">
+                      {sec} {secs}
+                    </p>
+                  )
+                })}
             </div>
           </div>
-          <button className="form-submit-button" onClick={this.handleStartTimer}>Save</button>
+          <button
+            className="form-submit-button"
+            onClick={this.handleStartTimer}
+          >
+            Save
+          </button>
         </div>
-      )
+      );
     }
-  }
+  };
 
   render = () => {
     return (
@@ -65,6 +103,6 @@ export default class Timer extends Component {
         <div className="overlay" />
         {this.renderTask()}
       </div>
-    )
-  }
+    );
+  };
 }
