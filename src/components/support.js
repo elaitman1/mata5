510f89c486bd79faed08c77b2f41def17d8a59dc
Danny Peng <dannyw.peng@gmail.com>
2019-08-14 02:39:15 -0700

    support component with styling

--- src/components/support.js
+++ src/components/support.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Support = props => {
-  return (
-    <div className="support-container">
-      Hello Help/Support!
-    </div>
-  )
+export default class Support extends Component {
+  state = {
+    supportMessage: ""
+  };
+
+  update = e => {
+    this.setState({ supportMessage: e.currentTarget.value });
+  };
+
+  sendHelpMessage = () => {
+    this.setState({ supportMessage: "" });
+  };
+
+  render = () => {
+    return (
+      <div className="support-container">
+        <h4>Hi, How Can We Help?</h4>
+        <textarea
+          className="support-form-textarea"
+          value={this.state.supportMessage}
+          placeholder="Please briefly summarize the issue you just encountered. You'll be asked for more details later."
+          onChange={this.update}
+        />
+        <button className="form-submit-button" onClick={this.sendHelpMessage}>
+          Send
+        </button>
+      </div>
+    );
+  };
 }
-
-export default Support;
