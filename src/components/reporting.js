e08e09c603c44a0a511d0cfc605b865cf6be5478
System Administrator <root@YenChis-MacBook-Pro.local>
2019-09-14 16:24:20 -0700

    fix job number per note

--- src/components/reporting.js
+++ src/components/reporting.js
@@ -36,6 +36,18 @@ export default class Reporting extends Component {
       toolpath: ["Preparation", "Edit Toolpath"]
     };
 
+    let chats = this.props.chats;
+    let latestJobPartDate = '1970/01/01';
+    let latestJob = "";
+    Object.keys(chats["Jobs"]).forEach(chatName => {
+      let chatObj = chats["Jobs"][chatName];
+      let startTime = chatObj.responses["Start Time"];
+       if (new Date(startTime) > new Date(latestJobPartDate)) {
+          latestJobPartDate = startTime;
+          latestJob = chatName
+       }
+    });
+
     let reportingObj = { Machining: {}, Preparation: {} };
     Object.keys(this.props.machine.reporting).forEach(prepType => {
       let prepVal = this.props.machine.reporting[prepType];
@@ -166,7 +178,7 @@ export default class Reporting extends Component {
       deviceid: this.props.machine.device_id,
       note: this.state.cells.Note,
       partnumber: "",
-      jobnumber: Object.keys(this.props.chats.Jobs)[this.state.prepCheckJobNum]
+      jobnumber: this.latestJob
     };
 
     fetch(url, {
@@ -186,7 +198,7 @@ export default class Reporting extends Component {
       headers: { "Content-Type": "application/x-www-form-urlencoded" }
     })
       .then(res => console.log(res))
-      .then(response => console.log("Success:", JSON.stringify(response)))
+      .then(response => console.log("Success:", Object.keys(this.props.chats.Jobs)))
       .catch(error => console.error("Error:", error));
   };
 
@@ -294,7 +306,7 @@ export default class Reporting extends Component {
           <div className="preparation-checklist-container">
             <h4>Reporting</h4>
             <h4>For Job# {" "}
-             {Object.keys(this.props.chats.Jobs)[this.state.prepCheckJobNum]}
+             {this.latestJob}
             </h4>
                   <header className="preparation-checklist-cells-container">
                     {this.renderCells()}
